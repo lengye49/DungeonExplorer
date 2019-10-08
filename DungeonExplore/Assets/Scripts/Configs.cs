785bffae94df8690e3274eee5947b5c0e8b3f5f0
SkyCn <lengye49@126.com>
2019-10-08 17:43:07 +0800

    测试git

--- DungeonExplore/Assets/Scripts/Configs.cs
+++ DungeonExplore/Assets/Scripts/Configs.cs
@@ -1,8 +1,8 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public static class Configs  {
 
-    public static Vector2[] DungenCellPos = new Vector2[]{
+    //public static Vector2[] DungenCellPos = new Vector2[]{
         new Vector2(-400,748),new Vector2(-140,750),new Vector2(130,750),new Vector2(400,750),
         new Vector2(-400,550),new Vector2(-140,550),new Vector2(130,550),new Vector2(400,550),
         new Vector2(-400,360),new Vector2(-140,360),new Vector2(130,360),new Vector2(400,360),
